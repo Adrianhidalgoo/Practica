cdf77afff01c50d34a456c900cf1b1a7d7691430
Adrianhidalgoo <151675699+Adrianhidalgoo@users.noreply.github.com>
2024-05-23 14:13:45 +0200

    Update hola.java

--- hola.java
+++ hola.java
@@ -1,9 +1,13 @@
 public class hola {
   public static void main (String[] args) {
     System.out.println("CUACK!, Hola soy Alberto");
-
+    System.out.print ("Puta realmadrid, puta real madrid")
     System.out.println("Hola mundo! Soy Javier. uwunya");
     System.out.println("Hola, mundo, soy Felix :D");
+    System.out.print ("Puta realmadrid, puta real madrid")
+    System.out.print ("Puta realmadrid, puta real madrid")
+    System.out.print ("Puta realmadrid, puta real madrid")
+    System.out.print ("Puta realmadrid, puta real madrid")
 
     System.out.println("Hola, mundo, soy Raul :D");
   }
